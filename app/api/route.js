47ca1b87687d0ab777db15ea63dd100382760d48
Felipe Vieira <luizfelipevieira01@gmail.com>
2024-10-01 14:37:49 +0000

    correction of env varibale at code

--- app/api/route.js
+++ app/api/route.js
@@ -1,52 +1,53 @@
-// app/api/route.js
 import { ConnectDB } from "@/lib/config/db";
 import TodoModel from "@/lib/config/models/TodoModel";
 import { NextResponse } from "next/server";
 
+const LoadDB = async () => {
+    await ConnectDB()
+}
+
+LoadDB()
+
 export async function GET(request) {
-    await ConnectDB();
-    const todos = await TodoModel.find({});
-    return NextResponse.json({ todos }, { status: 200 });
+    const todos = await TodoModel.find({})
+    return NextResponse.json({ todos })
 }
 
 export async function POST(request) {
-    await ConnectDB();
     const { title, description } = await request.json();
 
-    if (!title || !description) {
-        return NextResponse.json({ message: 'Title and description are required' }, { status: 400 });
+    if (title === '' || description === '') {
+        return NextResponse.json('Title and description are required', { status: 400 });
     }
 
     try {
         await TodoModel.create({ title, description });
-        return NextResponse.json({ message: "Todo Created" }, { status: 201 });
+        return NextResponse.json("Todo Created", { status: 200 });
     } catch (error) {
-        return NextResponse.json({ message: error.message }, { status: 500 });
+        return NextResponse.json(error.message, { status: 500 });
     }
 }
 
 export async function DELETE(request) {
-    await ConnectDB();
-    const mongoId = request.nextUrl.searchParams.get('mongoId');
+    const mongoId = await request.nextUrl.searchParams.get('mongoId');
 
     if (!mongoId) {
-        return NextResponse.json({ message: 'Mongo ID is required' }, { status: 400 });
+        return NextResponse.json('Mongo ID is required', { status: 400 });
     }
 
     try {
         await TodoModel.findByIdAndDelete(mongoId);
-        return NextResponse.json({ message: 'Todo Deleted' }, { status: 200 });
+        return NextResponse.json('Todo Deleted', { status: 200 });
     } catch (error) {
-        return NextResponse.json({ message: error.message }, { status: 500 });
+        return NextResponse.json(error.message, { status: 500 });
+
     }
+
 }
-
 export async function PUT(request) {
-    await ConnectDB();
-    const mongoId = request.nextUrl.searchParams.get('mongoId');
-
+    const mongoId = await request.nextUrl.searchParams.get('mongoId');
     if (!mongoId) {
-        return NextResponse.json({ message: 'Mongo ID is required' }, { status: 400 });
+        return NextResponse.json('Mongo ID is required', { status: 400 });
     }
 
     try {
@@ -55,12 +56,8 @@ export async function PUT(request) {
                 isCompleted: true
             }
         });
-        return NextResponse.json({ message: 'Todo Updated' }, { status: 200 });
+        return NextResponse.json('Todo Updated', { status: 200 });
     } catch (error) {
-        return NextResponse.json({ message: error.message }, { status: 500 });
+        return NextResponse.json(error.message, { status: 500 });
     }
-}
-
-export default function handler(req, res) {
-    return NextResponse.json({ message: 'Method Not Allowed' }, { status: 405 });
 }
